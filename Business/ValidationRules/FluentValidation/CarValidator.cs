e1296416eae12b1c2c0570b94a1ca83778f6a242
Aytunc <aytuncalptekin@gmail.com>
2024-05-06 16:35:50 +0300

    Autofac and Fluent Validation Added

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,38 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).NotEmpty();
+            RuleFor(c => c.CarName).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.CarName).Must(NameControl).WithMessage("Araç isimlerinde sayı olamaz");
+
+        }
+
+        private bool NameControl(string arg)
+        {
+            foreach (char c in arg)
+            {
+                if(char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+
+
+}
---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,21 @@
+﻿using Castle.DynamicProxy;
+using System.Reflection;
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+            // classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger))); //log altyapısı eklendikten sonra tüm methodlara log sistemini uygular
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+
+}
